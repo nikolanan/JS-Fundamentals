4dba702996c25f834d766fa2a64d47ac69c94366
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-11-09 23:22:38 +0200

    Delete extractFile.js

--- extractFile.js
+++
@@ -1,9 +0,0 @@
-function extract(text){
-    let startIndexName = text.lastIndexOf("\\")
-    let endIndexName = text.lastIndexOf(".")
-
-    console.log(`File name: ${text.substring(startIndexName+1,endIndexName)}`)
-    console.log(`File extension: ${text.substring(endIndexName +1)}`)
-
-}
-extract('C:\\Internal\\training-internal\\Template.pptx' )
